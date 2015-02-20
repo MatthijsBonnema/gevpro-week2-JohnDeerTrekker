9071a951a8ab52a95c7e8d27fa0818c00e95bef3
Jerunox <jeroen_wilkens@hotmail.com>
2015-02-20 11:44:58 -0800

    importen verbeterd, combobox werkt en errors weg. Nu alleen nog een vak waarin de vlag komt en koppeling met flag_color.py maken.

--- flag_ui.py
+++ flag_ui.py
@@ -4,22 +4,21 @@
 # Date: 2/13/15
 # Info: 
 
-from PyQt4.QtCore import SIGNAL
-from PyQt4.QtGui import QComboBox, QDialog, QDoubleSpinBox
-from PyQt4.QtGui import QApplication, QGridLayout, QLabel
+from PyQt4 import QtGui
 import sys
 import flag_color, country
 
 
-class Country(QDialog):
-    super(Country, self).__init__(parent)
-    grid = QGridLayout()
-    self.combobox = QComboBox()
-    self.combobox.addItems(country.Country.importlist())
-    grid.addWidget(self.combobox, 1, 0)
+class Country(QtGui.QWidget):
+    def __init__(self):
+        super(Country, self).__init__()
+        grid = QtGui.QGridLayout()
+        self.combobox = QtGui.QComboBox(self)
+        self.combobox.addItems(country.Country.importlist(self))
+        grid.addWidget(self.combobox, 1, 0)
 
-def main(QDialog):
-    app = QApplication(sys.argv)
+def main():
+    app = QtGui.QApplication(sys.argv)
     country = Country()
     country.show()
     app.exec_()
